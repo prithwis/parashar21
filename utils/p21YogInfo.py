7c67cc93209825071ba8f095251f54174fc7a6b2
Prithwis Mukerjee <prithwis@yantrajaal.com>
2022-06-17 11:37:12 +0530

    Update p21YogInfo.py

--- utils/p21YogInfo.py
+++ utils/p21YogInfo.py
@@ -4,7 +4,7 @@
 # Yog Texts, Conditions
 # BudhAdiya Yog
 BudhAdityaText = 'Budhaditya^ yog formed by the conjunction of Sun and Mercury.  Considered to be a very auspicious Yoga. ^ ref : https://www.astroyogi.com/kundli/yog'            
-BudhAditaCond = {'GConjunctsG2.Su' : {'$in': ['Me']}}       # Su conjunct Me   
+BudhAdityaCond = {'GConjunctsG2.Su' : {'$in': ['Me']}}       # Su conjunct Me   
 #
 #BudhAditya Yog without Mercury Retrograde
 BudhAdityaNRText = 'Budhaditya_NR^ yog formed by the conjunction of Sun and Mercury where Mercury NOT retrograde. ^ ref : https://www.astroyogi.com/kundli/yog'            
