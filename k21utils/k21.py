52b47c957ea767d5700b8588f57ed0de14040ede
Prithwis Mukerjee <prithwis@yantrajaal.com>
2026-09-25 06:59:08 +0530

    Update k21.py

--- k21utils/k21.py
+++ k21utils/k21.py
@@ -2,6 +2,8 @@ import os
 import requests
 from google.colab import userdata
 
+from IPython.display import display, Markdown
+
 SYSTEM_PROMPT = """
 You are Khana21, a source-grounded Jyotisha interpretation system.
 
@@ -77,35 +79,7 @@ def authenticateOpenAI():
 
         return None
 
-"""
-try:
-    # Load key from Colab Secrets into environment
-    api_key = userdata.get("OPENAI_API_KEY")
-    if not api_key:
-        raise ValueError("OPENAI_API_KEY not found in Colab Secrets")
 
-    os.environ["OPENAI_API_KEY"] = api_key
-    print("API key loaded ✔")
-
-    # Call OpenAI identity endpoint
-    headers = {
-        "Authorization": f"Bearer {api_key}"
-    }
-
-    resp = requests.get("https://api.openai.com/v1/me", headers=headers)
-    resp.raise_for_status()   # catches 401 / 403 / etc.
-
-    me = resp.json()
-    name = me.get("name", "N/A")
-    email = me.get("email", "N/A")
-
-    print(f"Logged in as {name} {email}")
-
-except Exception as e:
-    print("❌ OpenAI credential check failed")
-    print("Reason:", str(e))
-
-"""
 def read_txt(path):
     with open(path, "r", encoding="utf-8") as f:
         return f.read()
@@ -218,7 +192,7 @@ def LLM_Response(client,systemPrompt, caseContext, cQuestion, vsID) :
     )
     return(response)
     
-from IPython.display import display, Markdown
+
 
 def prettierPrint(response):
 
